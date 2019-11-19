985aedfd1ff7a5b44bb3188659c7ba610c2b28a0
jankapunkt <jkuester@uni-bremen.de>
2019-11-19 09:43:15 +0100

    cloze item renderer uses simple tokenizer

--- renderers/items/cloze/clozeItemRenderer.js
+++ renderers/items/cloze/clozeItemRenderer.js
@@ -1,35 +1,26 @@
+import { ReactiveVar } from 'meteor/reactive-var'
+import {createSimpleTokenizer} from '../../../utils/tokenizer'
 import './clozeItemRenderer.html'
 
-const inputRegex = new RegExp('{{.+}}', 'g')
-const openBracket = '{{'
-const closeBracket = '}}'
+const startPattern = '{{'
+const closePattern = '}}'
+const tokenize = createSimpleTokenizer(startPattern, closePattern)
+
+Template.clozeItemRenderer.onCreated(function () {
+  const instance = this
+  instance.tokens = new ReactiveVar()
+
+  instance.autorun(() => {
+    const data = Template.currentData()
+    const { value } = data
+    const tokens = tokenize(value)
+    instance.tokens.set(tokens)
+
+  })
+})
 
 Template.clozeItemRenderer.helpers({
   tokens () {
-    const { value } = Template.instance().data
-    if (!value) return
-
-    let startIndex = 0, endIndex = 0
-    const tkns = []
-    while (startIndex < value.length && startIndex > -1) {
-      startIndex = value.indexOf(openBracket, endIndex)
-
-      if (startIndex === -1 || startIndex >= value.length) {
-        // no open bracket is found, we still try to add the rest of the words
-        // that may remain in the value string until the end
-        tkns.push({ value: value.substring(endIndex, value.length) })
-        break
-      }
-
-      // add previous words
-      tkns.push({ value: value.substring(endIndex, startIndex) })
-
-      // add input related words
-      endIndex = value.indexOf(closeBracket, startIndex)
-      tkns.push({ isInput: true, value: value.substring(startIndex + openBracket.length, endIndex) })
-      endIndex += closeBracket.length
-    }
-    console.log(tkns)
-    return tkns
+    return Template.instance().tokens.get()
   }
 })
