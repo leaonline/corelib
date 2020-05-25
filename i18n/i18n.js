59fea9a31e5e499be759005d54df23dd2b8242d3
Jan Küster <jkuester@uni-bremen.de>
2020-05-25 09:08:31 +0200

    lang updated

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,14 +1,28 @@
 import { check } from 'meteor/check'
-
 let _translator
 
 export const i18n = {}
+
+async function autoLoadLocale (lang) {
+  switch (lang) {
+    case 'de':
+      return import('./i18n_de')
+    default:
+      return import('./i18n_de')
+  }
+}
 
 i18n.load = function ({ get, set, getLocale }) {
   check(get, Function)
   check(set, Function)
   check(getLocale, Function)
   _translator = { get, set, getLocale }
+  const locale = getLocale()
+  autoLoadLocale(locale)
+    .then(json => {
+      set(locale, json)
+    })
+    .catch(e => console.error(e))
 }
 
 i18n.get = function (...params) {
