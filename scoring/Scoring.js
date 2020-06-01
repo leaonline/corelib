5b9fc8c18112aa59f1d9c1b6882b9ee8505561c0
Jan Küster <jkuester@uni-bremen.de>
2020-06-01 12:05:22 +0200

    Scoring changed to inversion of control pattern

--- scoring/Scoring.js
+++ scoring/Scoring.js
@@ -1,20 +1,27 @@
-const scoringMap = new Map(Object.entries({
-  singleChoice: {
-    name: 'singleChoice',
-    score: function (itemDoc, responseDoc) {
-      const { scoring } = itemDoc
-      return scoring.map(({ competency, correctResponse }) => {
-        const value = responseDoc.responses[0]
-        const score = correctResponse === value
-        const isUndefined = value === Scoring.UNDEFINED
-        return { competency, correctResponse, value, score, isUndefined }
-      })
-    }
-  }
-}))
+const scoreFunctions = new Map()
+
+let _initialized = false
 
 export const Scoring = {
   name: 'scoring',
   UNDEFINED: '__undefined__',
-  get: key => scoringMap.get(key)
+  register: (key, fn) => scoreFunctions.set(key, fn),
+  run: function (key, itemDoc, responseDoc) {
+    const scoreFn = scoreFunctions.get(key)
+    if (!scoreFn) throw new Error(`Expected scorer by key <${key}>`)
+    return scoreFn(itemDoc, responseDoc, { isUndefined })
+  },
+  async init () {
+    if (_initialized) return true
+    const { SingleChoice } = await import('../contexts/items/SingleChoice')
+    Scoring.register(SingleChoice.name, SingleChoice.score)
+    _initialized = true
+    return true
+  }
 }
+
+// -------------------------------------------------
+// HELPER FUNCTIONS TO BE PASSED TO SCORE FUNCTIONS
+// -------------------------------------------------
+
+const isUndefined = value => typeof value === 'undefined' || value === null || value === Scoring.UNDEFINED
