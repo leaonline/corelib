7c551df19ceb456cf19b337179b13404d34dd093
Jan Küster <jkuester@uni-bremen.de>
2020-06-19 11:13:41 +0200

    utils formIsValid uses extended options object as third parameter

--- utils/form.js
+++ utils/form.js
@@ -1,5 +1,14 @@
 /* global AutoForm */
 
+const defaultCleanOptions = {
+  filter: true,
+  autoConvert: true,
+  removeEmptyStrings: true,
+  trimStrings: true,
+  getAutoValues: true,
+  removeNullsFromArrays: true
+}
+
 /**
  * Reactively checks a form for input validity by a given schema.
  * Side-effect: adds sticky validation errors if form is not valid
@@ -8,28 +17,32 @@
  * @param isUpdate
  * @return {Object|null} form document if form is valid, otherwise null
  */
-export const formIsValid = function formIsValid (formId, schema, isUpdate) {
+export const formIsValid = function formIsValid (formId, schema, { isUpdate, template, clean } = {}) {
   // xxx: in some rare cases on insert forms there is no return value from
   // AutoForm.getFormValues, so we trick the validation into using an empty doc
-  const formValues = AutoForm.getFormValues(formId) || { insertDoc: {}, updateDoc: {} }
+  const formValues = AutoForm.getFormValues(formId, template, schema) || { insertDoc: {}, updateDoc: {} }
   const formDoc = isUpdate
     ? formValues.updateDoc
     : formValues.insertDoc
+
   let options
   if (isUpdate) {
     options = {}
     options.modifier = true
+    options.clean = Object.assign({}, clean, defaultCleanOptions)
   }
+
   const context = schema.newContext()
   context.validate(formDoc, options)
+
   const errors = context.validationErrors()
   if (errors && errors.length > 0) {
     console.log(errors)
     errors.forEach(err => AutoForm.addStickyValidationError(formId, err.key, err.type, err.value))
     return null
-  } else {
-    return formDoc
   }
+
+  return formDoc
 }
 
 /**
