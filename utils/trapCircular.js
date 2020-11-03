c11f7e9c17d53866e9551f24b9418745704cf664
Jan Küster <jkuester@uni-bremen.de>
2020-11-03 14:34:41 +0100

    utils trapCircular added

---
+++ utils/trapCircular.js
@@ -0,0 +1,24 @@
+/**
+ * Wrap imports with potential to cause circular referencing issues in
+ * a function that return sthe desired result and pass thi function to this one.
+ *
+ * The function will only run when the internal proxy trap is accessed.
+ * @param fct
+ * @return {{}}
+ */
+export const trapCircular = function trapCircular (fct) {
+  return new Proxy({}, {
+    get: function (target, name, o) {
+      const data = fct()
+      console.log('get trap', target, name, o, data)
+
+      if (name === 'toJSON') {
+        return () => data
+      }
+
+      if (Object.prototype.hasOwnProperty.call(data, name)) {
+        return data[name]
+      }
+    }
+  })
+}
