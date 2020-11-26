b0e076d5ef6fb605b88c52a6cdf3fba4448a1055
Jan Küster <jkuester@uni-bremen.de>
2020-11-26 17:11:16 +0100

    schema tests implemented

---
+++ schema/SchemaOptions.js
@@ -0,0 +1,62 @@
+/**
+ * Definitions for extended schema options that can be defined by contexts
+ * in order to be a valid consumable resource for the backend application.
+ */
+export const SchemaOptions = {
+  unique: Boolean,
+  list: Boolean,
+
+  /**
+   * Defines, whether the given field contains a richtext, that can be styled.
+   */
+  richText: Boolean,
+
+  /**
+   * Defines, whether a field represents an url that is used to load a media
+   * resource.
+   */
+  isMediaUrl: Boolean,
+
+  /**
+   * Defines a dependency to an external document (located in the given
+   * collection), represented by a given field(-name).
+   */
+  dependency: {
+    collection: String,
+    field: String
+  },
+
+  /**
+   * Defines a computed value by a given input and a given processing method.
+   */
+  value: {
+    method: String,
+    input: Array
+  },
+
+  /**
+   * Support aldeed:autoform
+   */
+  autoform: Object,
+
+  /**
+   * a fixed set of options
+   */
+  options: Array,
+
+  /**
+   * grouping properties
+   */
+  group: String,
+
+  /**
+   * Indicates, that this field contains dynamically rendered content, that
+   * is assigned via editor and follows a custom schema.
+   */
+  isPageContent: Boolean,
+
+  /**
+   * Indicates, that the defined Array type is sortable in it's order.
+   */
+  isSortable: Boolean
+}
---
+++ schema/tests/SchemaOptions.test.js
@@ -0,0 +1,29 @@
+/* eslint-env mocha */
+import { Random } from 'meteor/random'
+import { expect } from 'chai'
+
+import {SchemaOptions} from '../SchemaOptions'
+
+describe('SchemaOptions', function () {
+  it ('keeps integrity of the structure', function () {
+    expect(SchemaOptions).to.deep.equal({
+      unique: Boolean,
+      list: Boolean,
+      richText: Boolean,
+      isMediaUrl: Boolean,
+      dependency: {
+        collection: String,
+        field: String
+      },
+      value: {
+        method: String,
+        input: Array
+      },
+      autoform: Object,
+      options: Array,
+      group: String,
+      isPageContent: Boolean,
+      isSortable: Boolean
+    })
+  })
+})
