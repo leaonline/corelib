96b3a784d92807d5d7f73845c8e8cd0881c6ab29
Jan Küster <jkuester@uni-bremen.de>
2021-03-29 09:42:43 +0200

    utils collection addCollection added

--- utils/collection.js
+++ utils/collection.js
@@ -1,17 +1,48 @@
 import { Mongo } from 'meteor/mongo'
 
+/** @private */
+const localCollections = new Map()
+
+/**
+ * Local Collections only! Adds a local collection to the current realm so it is
+ * accessible via {getCollection}.
+ * @param collection {Mongo.Collection} the LocalCollection to add
+ * @param name {String} name of the collection to be used for access
+ * @return {boolean} true if added, false if not
+ */
+export const addCollection = (collection, name) => {
+  // non-local collections are already registered
+  if (collection._name !== null) {
+    throw new Error(`Unexpected collection name. Expected null, got ${collection._name}`)
+  }
+
+  localCollections.set(name, collection)
+  return localCollections.has(name)
+}
+
 /**
  * Gets a collection either by name (String) or { name } (Object).
- * @param options Either a String or Object with name property
- * @return {Mongo.Collection}
+ * Main relation is the {collection._name} property.
+ * Note, that a {LocalCollection} is not covered out-of-the box and needs to be
+ * added via {addCollection}.
+ * Prioritizes local collections in favor of synced collections.
+ * @param name Either a String or Object with name property
+ * @return {Mongo.Collection|undefined} The collection by name or undefined.
  */
-export const getCollection = options => {
-  const type = typeof options
-  if (type === 'string') {
-    return Mongo.Collection.get(options)
+export const getCollection = name => {
+  const type = typeof name
+
+  if (type !== 'string' && type !== 'object') {
+    throw new Error(`Unexpected type for "name" -> ${type}, expected String or Object { name:String }`)
   }
-  if (type === 'object' && options.name && typeof options.name === 'string') {
-    return Mongo.Collection.get(options.name)
+
+  const ctxName = type === 'string'
+    ? name
+    : name.name
+
+  if (localCollections.has(ctxName)) {
+    return localCollections.get(ctxName)
   }
-  throw new Error(`Unexpected type for "name" -> ${type}, expected String or Object { name:String }`)
+
+  return Mongo.Collection.get(name)
 }
