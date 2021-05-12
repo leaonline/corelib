7b02777db2ec7030e69e68981de0bba99fbc5729
Jan Küster <jkuester@uni-bremen.de>
2021-05-12 16:41:19 +0200

    utils setProperty fix ownership check

--- utils/object/setProperty.js
+++ utils/object/setProperty.js
@@ -1,6 +1,6 @@
 export const setProperty = (obj, prop, value, { enumerable, writable, configurable } = {}) => {
-  if (prop in Object) {
-    throw new Error(`Attempted to set forbidden property [${prop}].`)
+  if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+    throw new Error(`Attempted to define already defined property [${prop}].`)
   }
 
   const options = { enumerable, configurable, writable, value }
